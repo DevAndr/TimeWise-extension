7bee8369bed6b1eb955b09572011ac5900bd8aaa
One <pitbullstar96@gmail.com>
2026-03-14 11:59:48 +0300

    change url api

--- src/manifest.ts
+++ src/manifest.ts
@@ -6,7 +6,7 @@ export default defineManifest({
     version: "1.0.0",
     description: "A Chrome extension to track and manage your time effectively.",
     permissions: ["storage", "tabs", "activeTab", "clipboardWrite", "idle", "alarms"],
-    host_permissions: ["http://localhost:3031/*"],
+    host_permissions: ["http://localhost:3031/*", "http://192.168.50.233:3031/*"],
     background: {
         service_worker: "src/background/background.ts",
         type: "module"
